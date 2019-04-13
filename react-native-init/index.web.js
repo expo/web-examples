541bc5d1dc375fd4c0915816c77747513edaf58a
Evan Bacon <baconbrix@gmail.com>
2019-04-13 00:50:36 -0700

    Updated to work with the latest expo-cli
    * Changed main -> root

--- react-native-init/index.web.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  rootTag: document.getElementById('main'),
-});
